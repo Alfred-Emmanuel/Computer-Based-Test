d4456cc876cf9d12bf4804efbd5852485897f15e
Alfred Emmanuel <ealfred828@gmail.com>
2024-04-17 18:09:23 +0100

    reduced the time allocated in the timer

--- src/Componennts/countdownTimerFunctionality.jsx
+++ src/Componennts/countdownTimerFunctionality.jsx
@@ -1,37 +1,9 @@
-// import dayjs from "dayjs";
-
-// export function getRemainingTimeUntilMsTimestamp(timestampMs){
-
-//     const timestampDayjs = dayjs(timestampMs)
-//     const nowDayjs = dayjs()
-//     return{
-//         seconds: getRemainingSeconds(nowDayjs, timestampDayjs),
-//         minutes: getRemainingMinutes(nowDayjs, timestampDayjs),
-//         hours: getRemainingHours(nowDayjs, timestampDayjs)
-//     }
-// }
-
-// function getRemainingSeconds(nowDayjs, timestampDayjs){
-//     const seconds = timestampDayjs.diff(nowDayjs, 'seconds') % 60
-//     return seconds
-// }
-
-// function getRemainingMinutes(nowDayjs, timestampDayjs){
-//     const minutes = timestampDayjs.diff(nowDayjs, 'minutes') % 60
-//     return minutes
-// }
-
-// function getRemainingHours(nowDayjs, timestampDayjs){
-//     const hours = timestampDayjs.diff(nowDayjs, 'hours') % 24
-//     return hours
-// }
-
 import React, { useState, useEffect } from 'react';
 
 const CountdownTimer = (props) => {
-    const [timeLeft, setTimeLeft] = useState(7200); // 2 hours in seconds
+    const [timeLeft, setTimeLeft] = useState(600); // 2 hours in seconds
 
-    const tenMinutesLeft = timeLeft <= 600
+    const fiveMinutesLeft = timeLeft <= 300
   
     useEffect(() => {
       const intervalId = setInterval(() => {
@@ -48,7 +20,7 @@ const CountdownTimer = (props) => {
     const seconds = timeLeft % 60;
   
     return (
-      <div className={tenMinutesLeft ? "breathing-timer" : ""}>
+      <div className={fiveMinutesLeft ? "breathing-timer" : ""}>
         <h1>{hours} Hour :{minutes} minutes :{seconds} seconds</h1>
       </div>
     );
